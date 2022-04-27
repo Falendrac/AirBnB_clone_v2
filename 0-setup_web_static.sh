611ffeae3a1fcb90dcfba58f74bf31508a3f3146
Falendrac <leger_franck@hotmail.fr>
2022-04-27 11:07:56 +0200

    Add task 0 of deployment.

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static.
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test
+echo "Hello darkness my old friend" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data
+sed -i '/server_name/ a\\n\tlocation \/hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+service nginx restart
